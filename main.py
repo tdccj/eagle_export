69d823a81039fb2d8001d4fa9120102b2d442f4d
tdccj <tudouccj@163.com>
2022-09-23 21:15:44 +0800

    1.3

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ import time
 
 
 def fenlei(type1):
-    global sourec_ft, b
+    global sourec_ft, b, v
     if type1 in ('.jpg', '.png', '.jpeg', '.webp'):
         if b >= 1:
+            pass
+        elif v > 1:
             pass
         else:
             print("照片")
@@ -19,8 +21,16 @@ def fenlei(type1):
         print("网页")
         sourec_ft = "url"
         b = 1
+
+    elif type1 in ('.mp4', '.mov', '.avi', '.mkv'):
+        print("视频")
+        sourec_ft = "video"
+        v = 1
+
     else:
         if b > 1:
+            pass
+        elif v > 1:
             pass
         else:
             print("无法分类")
@@ -35,13 +45,13 @@ def dabao(a, sft, num1, selfname, export, path):
     # 写入描述文件
     data_w = open(export + "\\" + sft + "\\" + str(num1) + "_" + sourec_name + ".txt", "w")
     print(export + "\\" + sft + "\\" + str(num1) + "_" + sourec_name + ".txt")
-    data_w.write(a[(a.find('"name":"')+8):a.find('","size"')])  # name
+    data_w.write(a[(a.find('"name":"') + 8):a.find('","size"')])  # name
     data_w.write("\n")
-    data_w.write(a[(a.find('"tags":[') + 8):a.find('],"folders"')])    # tag
+    data_w.write(a[(a.find('"tags":[') + 8):a.find('],"folders"')])  # tag
     data_w.write("\n")
-    data_w.write(a[(a.find('"url":"') + 7):a.find('","annotation"')])   # url
+    data_w.write(a[(a.find('"url":"') + 7):a.find('","annotation"')])  # url
     data_w.write("\n")
-    data_w.write(a[(a.find('"annotation":"') + 14):a.find('","modificationTime"')])   # annotation 注释
+    data_w.write(a[(a.find('"annotation":"') + 14):a.find('","modificationTime"')])  # annotation 注释
     data_w.write("\n")
     data_w.write("\n")
     data_w.write(a)
@@ -55,15 +65,26 @@ def dabao(a, sft, num1, selfname, export, path):
         shutil.copyfile(b, c)
 
 
+def trymkdir(dirtype):
+    try:
+        os.mkdir(export + "\\" + dirtype)
+    except FileExistsError:
+        pass
+
+
 sourec_ft = 0
 a = 0
 path_image = input("输入image文件夹路径")
 export = input("请输入输出文件夹")
 path = os.listdir(path_image)
 # print(path)
-os.mkdir(export + "\\" + "photo")
-os.mkdir(export + "\\" + "url")
-os.mkdir(export + "\\" + "other")
+
+# 尝试创建文件夹
+trymkdir("photo")
+trymkdir("video")
+trymkdir("other")
+trymkdir("url")
+
 num = -1
 for j in path:
     b = 0
