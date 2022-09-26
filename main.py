02f917b971c088da304e15e1b169989c2bd59939
tdccj <tudouccj@163.com>
2022-09-26 20:36:16 +0800

    2.0.1

--- main.py
+++ main.py
@@ -1,114 +1,53 @@
 # coding=utf-8
 # 此程序用于导出包括其他信息在内的eagle库
+# 第一次重构
 
 import os
 import shutil
-import time
+
+path_images = 'none'
+path_export = 0
+folder_images = []
+file_metadata = 'none'
 
 
-def fenlei(type1):
-    global sourec_ft, b, v
-    if type1 in ('.jpg', '.png', '.jpeg', '.webp'):
-        if b >= 1:
-            pass
-        elif v >= 1:
-            pass
-        else:
-            print("照片")
-            sourec_ft = "photo"
-
-    elif type1 in ('.url', '.db'):
-        print("网页")
-        sourec_ft = "url"
-        b = 1
-
-    elif type1 in ('.mp4', '.mov', '.avi', '.mkv'):
-        print("视频")
-        sourec_ft = "video"
-        v = 1
-
-    else:
-        if b >= 1:
-            pass
-        elif v >= 1:
-            pass
-        else:
-            print("无法分类")
-            sourec_ft = "other"
-
-
-def dabao(a, sft, num1, selfname, export, path):
-    # print(sft)
-    sourec_name = a[(a.find("name") + 7):(a.find("size") - 3)]
-    print(sourec_name)
-    num1 = num+2
-
-    # 写入描述文件
-    data_w = open(export + "\\" + sft + "\\" + str(num1) + "_" + sourec_name + ".txt", "w")
-    print(export + "\\" + sft + "\\" + str(num1) + "_" + sourec_name + ".txt")
-    data_w.write('#name: '+a[(a.find('"name":"') + 8):a.find('","size"')])  # name
-    data_w.write("\n")
-    data_w.write('#tag: '+a[(a.find('"tags":[') + 8):a.find('],"folders"')])  # tag
-    data_w.write("\n")
-    data_w.write('#url: '+a[(a.find('"url":"') + 7):a.find('","annotation"')])  # url
-    data_w.write("\n")
-    data_w.write('#annotation: '+a[(a.find('"annotation":"') + 14):a.find('","modificationTime"')])  # annotation 注释
-    data_w.write("\n")
-    data_w.write("\n")
-    data_w.write(a)
-    data_w.close()
-
-    # 复制源文件
-    for k in selfname:
-        b = path_image + "\\" + path[num] + "\\" + k
-        c = export + "\\" + sft + "\\" + str(num1) + "_" + k
-        print(b, c)
-        shutil.copyfile(b, c)
-
-
-def trymkdir(dirtype):
-    try:
-        os.mkdir(export + "\\" + dirtype)
-    except FileExistsError:
+# 寻找地址
+def looking_for():
+    global path_images, path_export
+    # path_images = input("输入image文件夹路径")
+    path_images = r'Z:\shu jv ku\军事.library\images'
+    if path_images[-6:] == "images":
         pass
+    else:
+        print("路径错误")
+    # path_export = input("请输入输出文件夹")
+    path_export = r'Z:\临时文件\导出测试'
 
 
-sourec_ft = 0
-a = 0
-path_image = input("输入image文件夹路径")
-export = input("请输入输出文件夹")
-path = os.listdir(path_image)
-# print(path)
+# 读取文件夹内信息
+def read_the_folder():
+    global folder_images
+    folder_images = os.listdir(path_images)
 
-# 尝试创建文件夹
-trymkdir("photo")
-trymkdir("video")
-trymkdir("other")
-trymkdir("url")
 
-num = -1
-for j in path:
-    b = 0
-    v = 0
-    self_name = []
+# 安装文件夹分类文件
+def classify_the_files():
+    global file_metadata, folder_images
 
-    path_jvbu = os.listdir(path_image + '\\' + path[num])
-    # print(num)
-    # print(path_jvbu)
-    for i in path_jvbu:
-        file_type = os.path.splitext(i)[1]
+    num = 0
+    for folder_image in folder_images:
+        path_image = path_images + '\\' + folder_images[num]      # 获取子文件夹路径
+        path_metadata = path_image + r"\\metadata.json"     #获取详情文件路径
+        with open(path_metadata,'r',encoding='utf-8') as metadata:
+            file_metadata = metadata.read()
+        print(file_metadata.find("size"))
 
-        if file_type == '.json':
-            # print("json")
-            # print(path_image + '\\' + path[num] + '\\' + i)
-            data = open(path_image + '\\' + path[num] + '\\' + i, 'r', encoding='utf-8')
-            a = data.read()
-            # print("a", a)
-            data.close()
 
-        elif file_type != '.json':
-            # print(file_type)
-            fenlei(file_type)
-            self_name.append(i)
-    dabao(a, sourec_ft, num, self_name, export, path)
-    num = num + 1
+
+def main():
+    looking_for()
+    read_the_folder()
+    classify_the_files()
+
+
+main()
